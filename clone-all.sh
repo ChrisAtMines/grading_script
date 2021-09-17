0d44b1a6e6744795216a698b888405be4e20e39c
mcbeej <mcbee@mines.edu>
2021-09-17 13:07:43 -0500

    Edited how roster.json is fed

--- clone-all.sh
+++ clone-all.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 ASSIGNMENT=$1
-ROSTER=$2
+
+if [ -z "$2" ]
+then 
+    ROSTER=roster.json
+else
+    ROSTER=$2
+fi
 
 echo $ASSIGNMENT
-NUM_STUDENTS=$(jq length roster.json)
+NUM_STUDENTS=$(jq length $ROSTER)
 
 for (( i=0; i<$NUM_STUDENTS; i++ ))
 do
-    SECTION=$(jq -r .[$i].section roster.json)
-    GIT_REPO_USER=$(jq -r .[$i].univ_username roster.json)
-    STUDENT_NAME_FIRST=$(jq -r .[$i].name_first roster.json)
-    STUDENT_NAME_LAST=$(jq -r .[$i].name_last roster.json)
+    SECTION=$(jq -r .[$i].section $ROSTER)
+    GIT_REPO_USER=$(jq -r .[$i].univ_username $ROSTER)
+    STUDENT_NAME_FIRST=$(jq -r .[$i].name_first $ROSTER)
+    STUDENT_NAME_LAST=$(jq -r .[$i].name_last $ROSTER)
 
     STUDENT_NAME=${STUDENT_NAME_FIRST// /_}_${STUDENT_NAME_LAST// /_}
     GIT_URL="https://github.com/mines-csci400/f21${SECTION}-user-${GIT_REPO_USER}-${ASSIGNMENT}"
